85580b46803d59bab5ab8e4a30921f0b27859253
Eloiza <eloizaams@yahoo.com.br>
2024-06-06 17:04:35 -0300

    builder with inner class

--- src/application/TestDesignPatterns.java
+++ src/application/TestDesignPatterns.java
@@ -1,23 +1,18 @@
 package application;
 
-import designPatterns.PersonBuilder;
 import entites.Person;
 
 public class TestDesignPatterns {
 	public static void main(String[] args) {
 		
-		Person person = new Person("Eloiza", "Souza", "eloizaams@yahoo.com.br", "111.222.333-44", "Barbacena");
-		
-		System.out.println(person);
-		
-		Person person2 = new PersonBuilder()
+		Person person = new Person.PersonBuilder()
 				.name("Eloiza")
 				.lastName("Souza")
 				.email("eloizaams@yahoo.com.br")
 				.hometown("Barbacena")
 				.id("111.222.333-44").build();
 		
-		System.out.println(person2);
+		System.out.println(person);
 		
 		
 		
--- src/entites/Person.java
+++ src/entites/Person.java
@@ -1,5 +1,6 @@
 package entites;
 
+
 public class Person {
 
 	private String name;
@@ -8,7 +9,8 @@ public class Person {
 	private String id;
 	private String hometown;
 
-	public Person(String name, String lastName, String email, String id, String hometown) {
+	 // Constructor private - must instance object using inner classs PersonBuilder
+	private Person(String name, String lastName, String email, String id, String hometown) {
 		this.name = name;
 		this.lastName = lastName;
 		this.email = email;
@@ -61,5 +63,40 @@ public class Person {
 		return "Person [name=" + name + ", lastName=" + lastName + ", email=" + email + ", id=" + id + ", hometown="
 				+ hometown + "]";
 	}
+
 	
+	public static class PersonBuilder {
+		
+		private String name;
+		private String lastName;
+		private String email;
+		private String id;
+		private String hometown;
+		
+		public PersonBuilder name(String name) {
+			this.name = name;
+			return this;
+		}
+		public PersonBuilder lastName(String lastName) {
+			this.lastName = lastName;
+			return this;
+		}
+		public PersonBuilder email(String email) {
+			this.email = email;
+			return this;
+		}
+		public PersonBuilder id(String id) {
+			this.id = id;
+			return this;
+		} 
+		public PersonBuilder hometown(String hometown) {
+			this.hometown = hometown;
+			return this;
+		}
+		
+		public Person build() {
+			return new Person(name, lastName, email, id, hometown);
+		}
+	
+	}
 }
