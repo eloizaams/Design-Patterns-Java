70568eba7eab6fd385ec47be5af440088de2b76a
Eloiza <eloizaams@yahoo.com.br>
2024-06-07 16:08:10 -0300

    reflection

--- src/application/TestDesignPatterns.java
+++ src/application/TestDesignPatterns.java
@@ -1,13 +1,26 @@
 package application;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import designPatterns.FactoryProduct;
 import entites.Person;
 import entites.Product;
 import services.TypeProduct;
 
 public class TestDesignPatterns {
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		
+		// REFLECTION - How to access a private constructor
+		Constructor<Person> constructorPerson = Person.class.getDeclaredConstructor(String.class, String.class, String.class, String.class, String.class);
+		constructorPerson.setAccessible(true);
+		Person personExpert = constructorPerson.newInstance("Expert","","","","");
+
+		System.out.println(personExpert);
+		
+
+		// Creating an object whith inner class builder
 		Person person = new Person.PersonBuilder()
 				.name("Eloiza")
 				.lastName("Souza")
@@ -17,13 +30,15 @@ public class TestDesignPatterns {
 		
 		System.out.println(person);
 		
-		
+		System.out.println();
 		Product fisicalProduct = new FactoryProduct().getInstance(TypeProduct.FISICAL);
 		
 		Product digitalProduct = new FactoryProduct().getInstance(TypeProduct.DIGITAL);
 		
 		System.out.println(fisicalProduct);
 		System.out.println(digitalProduct);
+		
+		
 		
 	}
 
